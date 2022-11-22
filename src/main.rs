8dd92b3b6702bdc64b79aea1e73383c0cde27e76
Andrew Yourtchenko <ayourtch@gmail.com>
2022-11-22 21:41:06 +0000

    remove some of the source code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
 use std::collections::HashMap;
 use std::time::{Duration, SystemTime};
 
+
+// comment out the next line to avoid crash
 use tide::Request;
-use tide::prelude::*;
 
-#[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u16,
+
+// use tide::prelude::*;
+
+async fn litir() -> () {
+    println!("hello2");
 }
-
-async fn litir_main() -> tide::Result<()> {
-    let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
-    app.listen("127.0.0.1:4000").await?;
-    Ok(())
-}
-
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
-}
-
 
 fn main() {
+    println!("hello");
 
-    // std::thread::sleep(std::time::Duration::from_secs(1));`
-    async_std::task::block_on(litir_main());
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    async_std::task::block_on(litir());
 }
